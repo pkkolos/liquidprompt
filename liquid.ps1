fcddd114453a494f0790f8416b7fa92a12da29f9
Panagiotis Kkolos <pkkolos@gmail.com>
2021-05-09 10:29:56 +0300

    Remove title tag feature and add back ps1 prefix and postfix
    The lp_title function can be used to perform the same functionality.

--- liquid.ps1
+++ liquid.ps1
@@ -8,8 +8,8 @@
 # Remember that most features come with their corresponding colors.
 
 # add time and jobs
-LP_PS1="${LP_TIME}${LP_JOBS}"
-LP_TITLE="${LP_JOBS}"
+LP_PS1="${LP_PS1_PREFIX}${LP_TIME}${LP_JOBS}"
+LP_TITLE="${LP_PS1_PREFIX}${LP_JOBS}"
 
 # add user, host and permissions colon
 LP_PS1+="${LP_BRACKET_OPEN}${LP_USER}${LP_HOST}${LP_PERM}"
@@ -22,10 +22,10 @@ LP_TITLE+="${LP_PWD}${LP_BRACKET_CLOSE}${LP_SCLS}${LP_VENV}${LP_PROXY}"
 LP_PS1+="${LP_VCS}"
 
 # add return code and prompt mark
-LP_PS1+="${LP_ERR}${LP_MARK_PREFIX}${LP_COLOR_MARK}${LP_MARK}"
-LP_TITLE+="${LP_ERR} ${LP_MARK}"
+LP_PS1+="${LP_ERR}${LP_MARK_PREFIX}${LP_COLOR_MARK}${LP_MARK}${LP_PS1_POSTFIX}"
+LP_TITLE+="${LP_ERR} ${LP_MARK}${LP_PS1_POSTFIX}"
 
 # Get the current prompt on the fly and make it a title
-_lp_title "${LP_TAG:-"$LP_TITLE"}"
+_lp_title "$LP_TITLE"
 
 # vim: set et sts=4 sw=4 tw=120 ft=sh:
